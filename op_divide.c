d7f97b25d08396a1439d877597b12f3a3064e88f
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-01-18 16:23:00 -0800

    monty

---
+++ op_divide.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * divide - Prints all the elements of the stack
+ * @stack: The stack to print
+ * @line_number: ...
+ *
+ * Return: Nothing
+ */
+void divide(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+	unsigned int d = 0, e = 0, length = 0;
+
+	length = count_stack(*stack);
+
+	if (length < 2)
+		handle_error(ERR_DIV_USG, NULL, line_number, NULL);
+
+	d = temp->n;
+
+	if (d == 0)
+		handle_error(ERR_DIV_ZRO, NULL, line_number, NULL);
+
+	e = temp->next->n;
+	temp->next->n = e / d;
+	*stack = temp->next;
+	free(temp);
+}
